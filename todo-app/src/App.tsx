94eaf99674f65e16e6bdde845747140203792178
ggoldJeongg <manage06@naver.com>
2024-04-22 22:39:45 +0900

    feat: 기본 회원가입 기능 구현
    - firebase의 기본 이메일, 비밀번호 생성 기능으로 계정 생성 구현
    - 생성 계정을 firestore에 저장하도록 했습니다.
    - 회원 가입 후 로그인 페이지로 이동하도록 했습니다.
    - 기본 CSS를 styled-components로 구현했습니다.
    - 페이지 경로 설정을 수정했습니다.

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import TodoList from "./components/todolist/TodoList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SignUp } from "./pages/signup/signUp";
+import { Login } from "./pages/signup/login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Router>
+        <Routes>
+          <Route path="/todolist" element={<TodoList />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
